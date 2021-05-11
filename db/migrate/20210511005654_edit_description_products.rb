677f63a658c8bb2e7e5b163ed66b753057afbddd
Brandy McKinney <missearlg80@gmail.com>
2021-05-10 21:26:32 -0400

    Created more migrations, added, changed and more

---
+++ db/migrate/20210511005654_edit_description_products.rb
@@ -0,0 +1,5 @@
+class EditDescriptionProducts < ActiveRecord::Migration[6.1]
+  def change
+    change_column :products, :description, :text
+  end
+end
---
+++ db/migrate/20210511010030_adding_products.rb
@@ -0,0 +1,5 @@
+class AddingProducts < ActiveRecord::Migration[6.1]
+  def change
+    add_column :products, :size, :integer
+  end
+end
